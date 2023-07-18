9633d408f95c73956806f7fe482a074bd72e3760
daadaa <wlaaldaadaa33@gmail.com>
2023-07-18 12:45:30 +0300

    somali_grils project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './component/header/Header'
+import Timer from './component/timer/Timer'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+      <Timer />
+      </div>
+  )
+}
+
+export default App
